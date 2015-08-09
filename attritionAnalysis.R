dd8e9d534d3ec71fa915c0f0ef6e6d5215453103
Raghunath T <traghunath98@gmail.com>
2015-08-09 22:04:27 +0530

    Some key changes for presentation on 7-Aug

--- attritionAnalysis.R
+++ attritionAnalysis.R
@@ -23,6 +23,8 @@ analyzeAttrition <- function(x_file = character()) {
 	aig_data <- read.csv(x_file, stringsAsFactors=FALSE)
 
 	aig_data <- convertToFactors(aig_data)
+    aig_data_sal <- subset(aig_data, !is.na(PCT_SAL))
+    
 	
 	#Plot attrition by competency, onsite/offshore / tenure
 	g <- ggplot(data=aig_data, aes(x=Competency, fill=Tenure_Yrs)) + geom_bar(stat="bin", position="stack") + facet_grid(Sex~Shore)
@@ -54,6 +56,48 @@ analyzeAttrition <- function(x_file = character()) {
 	g <- g + theme_bw() + theme(axis.text.x = element_text(size=6, angle=45), legend.text = element_text(size=7), legend.title=element_text(size=8), legend.position="bottom")
 	
 	g_reason_tenure <- g
+    
+    
+#Plot Reason and PCT_SAL, by competency
+
+    aig_reasonData_sal <- getSubset(aig_data_sal,"Reason",reason_codes)
+    aig_selectCompetency <- getSubset(aig_reasonData_sal,"Competency",c("C2","C3","C4","C5"))
+    g <- ggplot(data=aig_selectCompetency, aes(x=Reason, y=PCT_SAL)) + geom_boxplot() + facet_grid(.~Competency)
+    g <- g + labs(x="Reason", y="Salary Percentile", title="Reason(top 70%) and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x = element_text(size=6, angle=45))
+    
+    g_reason_sal <- g
+    
+#Plot Job Family and PCT_SAL, by competency
+    
+    aig_selectCompetency <- getSubset(aig_data_sal,"Competency",c("C2","C3","C4","C5"))
+    g <- ggplot(data=aig_selectCompetency, aes(x=Job_Family, y=PCT_SAL)) + geom_boxplot() + facet_grid(.~Competency)
+    g <- g + labs(x="Job Family", y="Salary Percentile", title="Job Family and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x = element_text(size=6, angle=45))
+    
+    g_jobFamily_sal <- g
+
+
+#Plot Job Family and sal_ratio, by competency
+
+    aig_selectCompetency <- getSubset(aig_data_sal,"Competency",c("C2","C3","C4","C5"))
+    g <- ggplot(data=aig_selectCompetency, aes(x=Job_Family, y=sal_ratio)) + geom_boxplot() + facet_grid(.~Competency)
+    g <- g + labs(x="Job Family", y="Salary (in lpa) / 1 Yr Total Exp", title="Job Family and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x = element_text(size=6, angle=45))
+
+    g_jobFamily_sal_ratio <- g
+    
+    
+#Plot Reason and sal_ratio, by competency
+
+    aig_reasonData_sal <- getSubset(aig_data_sal,"Reason",reason_codes)
+    aig_selectCompetency <- getSubset(aig_reasonData_sal,"Competency",c("C2","C3","C4","C5"))
+    g <- ggplot(data=aig_selectCompetency, aes(x=Reason, y=sal_ratio)) + geom_boxplot() + facet_grid(.~Competency)
+    g <- g + labs(x="Reason Code", y="Salary (in lpa) / 1 Yr Total Exp", title="Reason and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x = element_text(size=6, angle=45))
+    
+    g_reason_sal_ratio <- g
+
 
 #Plot competency - total experience
 
@@ -91,11 +135,25 @@ analyzeAttrition <- function(x_file = character()) {
 
     g_total_exp_tenure <- g
 
+# Plot relationship between pct_sal and tenure (in months) for C2-C5 competencies
+
+    g <- ggplot(data=getSubset(aig_data_sal,"Competency",c("C2","C3","C4","C5")), aes(x=PCT_SAL, y=Tenure/30, color=Sex)) + geom_point() + geom_smooth(method="lm") + scale_y_continuous(limits=c(0,150)) + facet_wrap(~Competency, ncol=2)
+    g <- g + labs(x="Salary Percentile", y="Tenure (months)", title="Tenure and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x=element_text(size=8),legend.text=element_text(size=7),legend.title=element_text(size=8), legend.position="bottom")
+
+    g_sal_tenure <- g
+
+# Plot relationship between sal_ratio and tenure (in months) for C2-C5 competencies
+
+    g <- ggplot(data=getSubset(aig_data_sal,"Competency",c("C2","C3","C4","C5")), aes(x=sal_ratio, y=Tenure/30, color=Sex)) + geom_point() + geom_smooth(method="lm") + scale_y_continuous(limits=c(0,150)) + facet_wrap(~Competency, ncol=2)
+    g <- g + labs(x="Salary (lpa) per 1 Yr of Exp", y="Tenure (months)", title="Tenure and Salary (Offshore)")
+    g <- g + theme_bw() + theme(axis.text.x=element_text(size=8),legend.text=element_text(size=7),legend.title=element_text(size=8), legend.position="bottom")
+
+    g_sal_ratio_tenure <- g
 
 
-
-
-x_attr <<-aig_data
+    x_attr <<-aig_data
+    x_attr_sal <<- aig_data_sal
 
 
 	fileName <- paste("AIG_Attrition",strftime(Sys.time(),"%d%b%y-%H%M"),".pdf", sep="")
@@ -106,15 +164,74 @@ x_attr <<-aig_data
 	pdf(fileName)
        	
 		print(g_exp_tenure)
-		print(g_total_exp_tenure)
-		print(g_competency_tenure)
+        print(g_total_exp_tenure)
+        print(g_sal_tenure)
+        print(g_sal_ratio_tenure)
+        print(g_reason_sal)
+        print(g_jobFamily_sal)
+        print(g_jobFamily_sal_ratio)
+        print(g_reason_sal_ratio)
+        print(g_competency_tenure)
 		print(g_location_tenure)
 		print(g_reason_tenure)
 		print(g_competency_exp)
-        	print(g_exitDate_Reason)
+        print(g_exitDate_Reason)
  
-				
 	dev.off()
+    
+    # Prepare the individual files for the respective slides
+    png("g_exp_tenure.png")
+        print(g_exp_tenure)
+    dev.off()
+    
+    png("g_total_exp_tenure.png")
+        print(g_total_exp_tenure)
+    dev.off()
+
+    png("g_sal_tenure.png")
+        print(g_sal_tenure)
+    dev.off()
+    
+    png("g_sal_ratio_tenure.png")
+        print(g_sal_ratio_tenure)
+    dev.off()
+
+    png("g_reason_sal.png")
+        print(g_reason_sal)
+    dev.off()
+
+    png("g_jobFamily_sal.png")
+        print(g_jobFamily_sal)
+    dev.off()
+    
+    png("g_jobFamily_sal_ratio.png")
+        print(g_jobFamily_sal_ratio)
+    dev.off()
+    
+    png("g_reason_sal_ratio.png")
+        print(g_reason_sal_ratio)
+    dev.off()
+    
+    png("g_competency_tenure.png")
+        print(g_competency_tenure)
+    dev.off()
+    
+    png("g_location_tenure.png")
+        print(g_location_tenure)
+    dev.off()
+    
+    png("g_reason_tenure.png")
+        print(g_reason_tenure)
+    dev.off()
+
+    png("g_competency_exp.png")
+        print(g_competency_exp)
+    dev.off()
+
+    png("g_exitDate_reason.png")
+        print(g_exitDate_reason)
+    dev.off()
+
 }
 
 ## This function takes the attrition dataframe and converts some of the key columns into factors
@@ -136,9 +253,14 @@ convertToFactors <- function(aig_data){
 	aig_data$Sex <- as.factor(aig_data$Sex)
 	aig_data$LOC <- as.factor(aig_data$LOC)
 	aig_data$Shore <- as.factor(aig_data$Shore)
+    aig_data$Job_Family <- as.factor(aig_data$Job_Family)
+    aig_data$Rating <- as.factor(aig_data$Rating)
     
     #Adding a new column called Exit Date
     aig_data$exitDate <- strptime(aig_data$DOL, "%d-%b-%y")
+
+    #Adding a new column called sal_ratio
+    aig_data$sal_ratio <- (aig_data$CTC/100000)/(aig_data$Total_Exp/12)
 
 	return(aig_data)
 }
